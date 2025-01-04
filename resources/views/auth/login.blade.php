8750e6c3bcaf884ef664a5aa02c549284d875580
alwin.zawadzki <alwin_zawadzki@ens.univ-artois.fr>
2025-01-05 00:21:54 +0100

    Ajout de la fonctionnalité de réinitialisation du mot de passe

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,9 @@
 <div class="wrap">
+    @if (session('message'))
+        <div class="alert alert-warning">
+            {{ session('message') }}
+        </div>
+    @endif
     <form class="login-form" action="{{route('login')}}" method="post">
         @csrf
         <div class="form-header">
@@ -13,12 +18,20 @@
         <div class="form-group">
             <input type="password" name="password" class="form-input" placeholder="password">
         </div>
+        <!--Remember Me Checkbox-->
+        <div class="form-group">
+            <input type="checkbox" name="remember" id="remember">
+            <label for="remember">Se souvenir de moi</label>
+        </div>
         <!--Login Button-->
         <div class="form-group">
             <button class="form-button" type="submit">Login</button>
         </div>
         <div class="form-footer">
             Vous n'avez pas de compte ? <a href="{{route('register')}}">Enregistrement</a>
+        </div>
+        <div class="form-footer">
+            <a href="{{ route('password.request') }}">Mot de passe oublié ?</a>
         </div>
     </form>
 </div><!--/.wrap-->
