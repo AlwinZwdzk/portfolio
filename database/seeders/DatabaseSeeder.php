24b68425fd1138fbc5a945cab156bc047b9853c5
alwin.zawadzki <alwin_zawadzki@ens.univ-artois.fr>
2025-01-06 09:36:06 +0100

    Ajout des roles admin et user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Enums\Role;
 use App\Models\Avis;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +17,14 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory([
+            'name' => "Robert Duchmol",
+            'email' => "robert.duchmol@domain.fr",
+            'email_verified_at' => now(),
+            'password' => Hash::make('GrosSecret'),
+            'remember_token' => Str::random(10),
+            'role' => Role::ADMIN,
+        ])->create();
         Avis::factory(10)->create();
     }
 }
